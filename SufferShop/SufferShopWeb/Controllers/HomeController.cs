e934070b6fb483c6f0d596ded604b14bea99e632
Vincent Robert Weis <vrobweis@gmail.com>
2020-11-05 07:17:06 -0800

    Added more notes and example code about ASP.Net. Need to make separate repo.

--- SufferShop/SufferShopWeb/Controllers/HomeController.cs
+++ SufferShop/SufferShopWeb/Controllers/HomeController.cs
@@ -18,8 +18,14 @@ namespace SufferShopWeb.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        
+        public IActionResult Index(int id = -1, string name="defaultname")
         {
+
+            ViewBag.id = id;
+            ViewBag.name = name;
+
+
             // Passing viewdata to the Index.cshtml file
             Customer customer = new Customer() { Id = 1, alias = "The Ultimate Customer", name = "Mr. Customer" };
             ViewData["CustomerUsingViewData"] = customer;
@@ -27,10 +33,23 @@ namespace SufferShopWeb.Controllers
             return View(customer);
         }
 
+        // This following line of code is attribute-based routing.
+        // Make sure it doesn't conflict with the conventional routing in the Startup.cs
+        [Route(("Controller=Home/action=Privacy/"))]
         public IActionResult Privacy()
         {
             return View();
         }
+
+        // You don't NEED to specify the action if it's in the right controller already.
+        [HttpPost]
+        [Route(("Controller=Home"))]
+        public IActionResult About()
+        {
+            return View();
+        }
+
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
